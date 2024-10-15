98ada45b39673dc9a1fbf5c9b91cc5dcab03b457
CyberPokemon <imoncoding@gmail.com>
2024-10-16 00:29:09 +0530

    validating expression method added

--- PolStringConvertor/PolStringConvertor.py
+++ PolStringConvertor/PolStringConvertor.py
@@ -4,7 +4,36 @@ __precedence={'+':1,'-':1,
               '*':2,"/":2,"%":2,
               "^":3}
 
+def isExpressionValid(expression):
+    stack=[]
+    previous_char=""
+    operators=set("+-*/%^")
+
+    for i, current_char in enumerate(expression):
+        if current_char=='(':
+            stack.append("(")
+        elif current_char==')':
+            if stack and stack[-1]=='(':
+                stack.pop()
+            elif not stack or stack[-1]!='(':
+                return False
+        
+        if current_char in operators:
+            if i==0:
+                return False
+            if previous_char in operators:
+                return False
+        previous_char=current_char
+    if stack:
+        return False
+    if expression[-1] in operators:
+        return False
+    
+    return True
+
 def infixToPostfix(infixexpression):
+    if not isExpressionValid(infixexpression):
+        return []
     infixexpression=infixexpression+')'
     infixexpression2=st(infixexpression,"+-*/%()",True)
     stack=['(',]
